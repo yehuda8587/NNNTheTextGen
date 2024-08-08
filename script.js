a1d5707ddb276f0dcfc32170553fc84a52082e6f
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2024-08-08 12:30:40 -0400

    Update script.js
    update on avologi victoria and woodgrove insteadof price

--- script.js
+++ script.js
@@ -1695,7 +1695,7 @@
                                     upDatedActualSpaPhone = "Can't find our spa? Give us a call at (236) 258-6757";
                                     treatmentSelected = "Natural Facelift Treatment";
                                     newSpaPhoneNumberSelected = "(236) 258-6757";
-                                    insteadOfSpa = "$399";
+                                    insteadOfSpa = "$377";
                                     break; 
 
                                     case "AVOLOGI Eneo Woodgrove":
@@ -1807,7 +1807,7 @@
                                     upDatedActualSpaPhone = "Can't find our spa? Give us a call at (236) 258-6757";
                                     treatmentSelected = "Natural Facelift Treatment";
                                     newSpaPhoneNumberSelected = "(236) 258-6757";
-                                    insteadOfSpa = "$399";
+                                    insteadOfSpa = "$377";
                                     break; 
 
 
