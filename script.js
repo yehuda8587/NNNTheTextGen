76928b2ba93f4e3a16c27e6b8152b65f50133261
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2024-09-13 03:07:28 -0400

    Update script.js
    added new malka promos

--- script.js
+++ script.js
@@ -7303,6 +7303,22 @@
                                         insteadOfSpa = "$377";
                                         break;
 
+                                        case "Malka Med Spa 3 in 1 Skin Tightening 79.95":
+                                        upDatedSpaName = "Malka Med Spa";
+                                        upDatedSpaAddress = "2601 Little Elm Parkway, Little Elm, TX, 75068"+"<br>"+"(Unit 1801, Building 18)"+"<br>"+"(Parking is available just in front of the spa)";
+                                        upDatedSpaLINK = " ";
+                                        upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/malka-med-spa-3-in-1-skin-tightening-treatment-79-95";
+                                        upDatedSpaPhone = "(469) 487-7573";
+                                        upDatedDurationTotal = "60 Minutes";
+                                        upDatedDurationOne = "- 15 minutes Cleansing";
+                                        upDatedDurationTwo = "- 45 minutes 3 in 1 Skin Tightening Treatment and Consultation"
+                                        promoPrice = "69.95";
+                                        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (469) 888-6616";
+                                        treatmentSelected = "3 in 1 Skin Tightening Treatment";
+                                        newSpaPhoneNumberSelected = "(469) 888-6616";
+                                        insteadOfSpa = "$377";
+                                        break;
+
                                         case "Lavelier Grand Junction LED 79.95":
                                         upDatedSpaName = "Lavelier";
                                         upDatedSpaAddress = "Mesa Mall, 2424 US-6, Grand Junction, CO 81505"+"<br>"+"(in the aisle of Target next to Verizon)"+"<br>"+"(The entrance by Target is the closest)";
