38d225f3067884850d12995becf87f4628208c5c
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-03-20 10:30:35 -0400

    Update script.js
    added new malka promo

--- script.js
+++ script.js
@@ -8555,6 +8555,23 @@
                                         insteadOfSpa = "$377";
                                         break;
 
+                                        case "Malka Med Spa Neck and Jawline 69.95":
+                                        upDatedSpaName = "Malka Med Spa";
+                                        upDatedSpaAddress = "2601 Little Elm Parkway, Little Elm, TX, 75068"+"<br>"+"(Unit 1801, Building 18)"+"<br>"+"(Parking is available just in front of the spa)";
+                                        upDatedSpaLINK = " ";
+                                        upDatedSpaMapLINK = "https://maps.app.goo.gl/Nv4x7HUjHc6g38Jq6";
+                                        upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/malka-med-spa-neck-amp-jawline-tightening-promo-69-95";
+                                        upDatedSpaPhone = "(469) 884-0553";
+                                        upDatedDurationTotal = "60 Minutes";
+                                        upDatedDurationOne = "- 45 minutes light cleansing and the Neck and Jawline treatment";
+                                        upDatedDurationTwo = "- 15 minutes Consultation"
+                                        promoPrice = "69.95";
+                                        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (469) 888-6616";
+                                        treatmentSelected = "Neck and Jawline Tightening";
+                                        newSpaPhoneNumberSelected = "(469) 888-6616";
+                                        insteadOfSpa = "$299";
+                                        break;
+
                                         case "Malka Med Spa Collagen 69.95":
                                         upDatedSpaName = "Malka Med Spa";
                                         upDatedSpaAddress = "2601 Little Elm Parkway, Little Elm, TX, 75068"+"<br>"+"(Unit 1801, Building 18)"+"<br>"+"(Parking is available just in front of the spa)";
