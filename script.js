d3673452cfea2b4b5d989087751a5918db808bda
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-04-17 14:56:38 -0400

    Update script.js
    added new promos

--- script.js
+++ script.js
@@ -10177,6 +10177,23 @@
                                             insteadOfSpa = "$377";
                                             break;
 
+                                            case "Skin Totale Neck and Jawline Lifting $59.95":
+                                            upDatedSpaName = "Skin Totale";
+                                            upDatedSpaAddress = "309 E Katella Avenue, Orange, CA 92867"+"<br>"+"(Inside Katella Shopping Center, behind Marie Callender's Restaurant & Bakery, near 'Train with Dave')";
+                                            upDatedSpaLINK = " ";
+                                            upDatedSpaMapLINK = "https://maps.app.goo.gl/f11qLG86Ze123w1aA";
+                                            upDatedSpaWebLINK = "https://www.firstouchbeauty.com/pages/skin-totale-med-spa-neck-amp-jawline-promo-59-95";
+                                            upDatedSpaPhone = "(714) 450-6547";
+                                            upDatedDurationTotal = "60 Minutes";
+                                            upDatedDurationOne = "- 45 minutes Skin Analysis and Neck and Jawline Lifting Treatment";
+                                            upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+                                            promoPrice = "59.95";
+                                            upDatedActualSpaPhone = "Can't find our spa? Give us a call at (714) 852-5300";
+                                            treatmentSelected = "Neck and Jawline Lifting Treatment";
+                                            newSpaPhoneNumberSelected = "(714) 852-5300";
+                                            insteadOfSpa = "$377";
+                                            break;
+
                                                 case "Lift and Revive Med Spa Natural Facelift $79.95":
                                                 upDatedSpaName = "Lift and Revive Med Spa";
                                                 upDatedSpaAddress = "Residences At La Cantera"+"<br>"+"6215 Via La Cantera San Antonio, TX 78256"+"<br>"+"(STE 0121 - Our Storefront is at the Residents At La Cantera)";
@@ -10947,7 +10964,7 @@
                                     upDatedSpaAddress = "2250 Kalakaua Ave. Honolulu, HI 96815"+"<br>"+"(Waikiki shopping plaza, 2nd floor next to the elevator, Room 217)";
                                     upDatedSpaLINK = "https://forms.gle/17kGFgn6KXJcs3xY7";
                                     upDatedSpaMapLINK = "https://maps.app.goo.gl/fFYEqgHUbmanrLiR6";
-                                    upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/moana-crystal-skin-tightening-69-95";
+                                    upDatedSpaWebLINK = "https://www.firstouchbeauty.com/pages/moana-crystals-skin-tightening-promo-69-95";
                                     upDatedSpaPhone = "(808) 764-3367";
                                     upDatedDurationTotal = "60 Minutes";
                                     upDatedDurationOne = "- 10 minutes Cleansing";
