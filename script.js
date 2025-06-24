c151c40980852c34036343232446100a573fb7c4
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-06-24 12:38:02 -0400

    Update script.js
    edit alpharetta

--- script.js
+++ script.js
@@ -11868,7 +11868,7 @@ function updateForm(){
 
                                 case "Below Zero Alpharetta Spa Natural Facelift $79.95":
                                 upDatedSpaName = "Below Zero Alpharetta";
-                                upDatedSpaAddress = "1000 North Point Cir, Alpharetta, GA 30022"+"<br>"+"(NORTH POINT MALL - our spa located at the first floor between Image is Everything Salon and Emani maternity store best entrance is the one near H&M or Von Maur)";
+                                upDatedSpaAddress = "1000 North Point Cir, Alpharetta, GA 30022"+"<br>"+"(NORTH POINT MALL - our spa located at the first floor between Image is Everything Salon and Emani maternity store best entrance is the one near Von Maur)";
                                 upDatedSpaLINK = "https://forms.gle/sQzmJF2atr2V2RJd9";
                                 upDatedSpaMapLINK = "https://maps.app.goo.gl/VnR27TgpeZLKVuhU7";
                                 upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/below-zero-alpharetta-natural-facelift-79-95-377";
