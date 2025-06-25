af324ca2bc613e0c274771853314e4b0ede44373
Miriam-22him <miriamapv@noneedleneeded.com>
2025-06-25 00:21:19 -0600

    script.js

--- script.js
+++ script.js
@@ -3663,7 +3663,7 @@ function updateForm(){
                                 insteadOfSpa = "$377";
                                 break;
 
-                                case "Tierra Med Spa Facelift Treatment 69.95-399":
+                                case "Tierra Med Spa Natural Facelift Treatment 69.95-399":
                                     upDatedSpaName = "Tierra Med Spa";
                                 upDatedSpaAddress = "The Beaches Toronto ," + "<br>" + "1912 Queen St. E. Toronto ON M4L1H5";
                                 upDatedSpaLINK = "https://forms.gle/JpNRkQvGJnqLgLrs6";
