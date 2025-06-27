b9dbe78c7b8af4a1e5e572d0a12bc31eab24230d
Miriam-22him <miriamapv@noneedleneeded.com>
2025-06-27 12:52:37 -0600

    script.js

--- script.js
+++ script.js
@@ -13674,6 +13674,23 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
+                                case "Tresor Beauty Tallahassee Bye Bye Saggy Eyes $79.95":
+                                upDatedSpaName = "Tresor Beauty";
+                                upDatedSpaAddress = "Governor Square Mall"+"<br>"+"1500 Apalachee Pkwy, Unit 2350, Tallahassee, FL 32301"+"<br>"+"(2nd floor, right above the Food Court, near Helzberg Jewelers and across Bath & Body Works)";
+                                upDatedSpaLINK = "https://forms.gle/d9bVoE3hSMELQ7a47";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/dHywqo21iYKv9GV16";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/tresor-beauty-bye-bye-saggy-eyes-79-95-377";
+                                upDatedSpaPhone = "(443)-972-0601";
+                                upDatedDurationTotal = "60 Minutes";
+                                upDatedDurationOne = " - 15 minutes Cleansing";
+                                upDatedDurationTwo = " - 30 minutes Bye Bye Saggy Eyes Treatment and 15 minutes Consultation";
+                                promoPrice = "79.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (443)-972-0601";
+                                treatmentSelected = "Bye Bye Saggy Eyes Treatment";
+                                newSpaPhoneNumberSelected = "(571) 325-9317";
+                                insteadOfSpa = "$377"
+                                break;
+
                                 case "Bee and Co Fernandina Beach Skin Tightening $79.95":
                                 upDatedSpaName = "Bee and Co";
                                 upDatedSpaAddress = "103 Centre St. Fernandina Beach, FL 32034"+"<br>"+"(Main Street)";
