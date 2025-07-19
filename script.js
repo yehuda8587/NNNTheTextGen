52630c65dfc200912ac305bd9b8c68ba8b6668ce
Miriam-22him <miriamapv@noneedleneeded.com>
2025-07-19 16:49:20 -0600

    script.js

--- script.js
+++ script.js
@@ -4629,6 +4629,24 @@ function updateForm(){
                                 insteadOfSpa = "$299";
                                 break;
 
+
+                                case "Ben's Beauty Bye Bye Saggy Eyes $59.95":
+                                upDatedSpaName = "Ben's Beauty Spa";
+                                upDatedSpaAddress = "20 N Raymond Ave, Pasadena CA 91103,"+"<br>"+"(Entrance is the door just left of Jimmy John's, 2nd Floor, Suite 250)"+"<br>"+"Door Code: 250"+"<br>"+"Use the door code, we'll buzz you in via intercom. Pull the door open after the buzz."+"<br>"+"Park at metered spots in front or park at 48 N Raymond Ave Parking Garage.";
+                                upDatedSpaLINK = "https://form.jotform.com/232685044919060";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/iYXCTAucg3a6bjnr5";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/bens-beauty-bye-bye-saggy-eyes-59-95-377" ;
+                                upDatedSpaPhone = "(626) 225-0556";
+                                upDatedDurationTotal = "60 Minutes";
+                                upDatedDurationOne = "20 minute Cleansing ";
+                                upDatedDurationTwo = "40 minutes of Bye Bye Saggy Eyes Treatment and Consultation";
+                                promoPrice = "59.95"
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (626) 298-6456";
+                                treatmentSelected = "Bye Bye Saggy Eyes Treatment";
+                                newSpaPhoneNumberSelected = "(626) 298-6456";
+                                insteadOfSpa = "$377";
+                                break;
+
                                 case "Ben's Beauty Spa Hydraglow Treatment 79.95":
                                 upDatedSpaName = "Ben's Beauty Spa";
                                 upDatedSpaAddress = "20 N Raymond Ave, Pasadena CA 91103,"+"<br>"+"(Entrance is the door just left of Jimmy John's, 2nd Floor, Suite 250)"+"<br>"+"Door Code: 250"+"<br>"+"Use the door code, we'll buzz you in via intercom. Pull the door open after the buzz."+"<br>"+"Park at metered spots in front or park at 48 N Raymond Ave Parking Garage.";
