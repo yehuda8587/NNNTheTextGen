d4d52dde9df55cc994b2291358a012e3daac40fb
Miriam-22him <miriamapv@noneedleneeded.com>
2025-07-31 14:19:01 -0600

    script.js

--- script.js
+++ script.js
@@ -14155,19 +14155,19 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
-                                case "Velora Med Spa Natural Facelift $79.95- Tagalog":
+                                case "Velora Med Spa Skin Tightening $79.95- Tagalog":
                                 upDatedSpaName = "Velora Med Spa";
                                 upDatedSpaAddress = "108 N State St, Chicago, IL 60602, United States"+"<br>"+"(Block 37, First level)";
                                 upDatedSpaLINK = "https://forms.gle/m4QFVnRBasHH7UqS9";
                                 upDatedSpaMapLINK = "https://maps.app.goo.gl/dV13uj69NZsR9zXZ9";
-                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/velora-med-spa-tg-natural-facelift-79-95-377";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/velora-med-spa-tg-skin-tigtening-79-95-377";
                                 upDatedSpaPhone = "(312) 625-1275";
                                 upDatedDurationTotal = "45 Minutes";
                                 upDatedDurationOne = " - 15 minutes Cleansing";
-                                upDatedDurationTwo = " - 30 minutes Natural Facelift & Consultation";
+                                upDatedDurationTwo = " - 30 minutes Skin Tightening & Consultation";
                                 promoPrice = "79.95";
                                 upDatedActualSpaPhone = "Can't find our spa? Give us a call at (331) 600-2304";
-                                treatmentSelected = "Natural Facelift";
+                                treatmentSelected = "Skin Tightening";
                                 newSpaPhoneNumberSelected = "(331) 600-2304";
                                 insteadOfSpa = "$377"
                                 break;
