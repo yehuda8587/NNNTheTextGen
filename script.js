ec492a7efa0beea2c4f7764fdeff79172bafb268
Miriam-22him <miriamapv@noneedleneeded.com>
2025-08-08 11:31:46 -0600

    script.js

--- script.js
+++ script.js
@@ -13934,6 +13934,23 @@ function updateForm(){
                                 insteadOfSpa = "$377";
                                 break; 
 
+                                case "The Soap Tree Omaha Neck and Jawline $59.95":
+                                upDatedSpaName = "The Soap Tree";
+                                upDatedSpaAddress = "10000 California St. Omaha, NE 68114" + "<br>" + "(STE 1221, Westroads Mall, 1st Floor across Five Below)";
+                                upDatedSpaLINK = "https://forms.gle/yXFkQsA4PmN61qxT8";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/ptdDexcxmHAWC65Y6";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/soap-tree-omaha-neck-and-jawline-59-95";
+                                upDatedSpaPhone = "(402) 252-3842";
+                                upDatedDurationTotal = "60 Minutes";
+                                upDatedDurationOne = "- 15 minutes Cleansing and 30 minutes Neck and Jawline Treatment";
+                                upDatedDurationTwo = "- 15 minutes Consultation";
+                                promoPrice = "59.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (928) 399-9242";
+                                treatmentSelected = "Neck and Jawline Treatment";
+                                newSpaPhoneNumberSelected = "(928) 399-9242";
+                                insteadOfSpa = "$377";
+                                break; 
+
             case "The Soap Tree Omaha Collagen Enhancing Treatment $69.95":
                                 upDatedSpaName = "The Soap Tree";
                                 upDatedSpaAddress = "10000 California St. Omaha, NE 68114" + "<br>" + "(STE 1221, Westroads Mall, 1st Floor across Five Below)";
