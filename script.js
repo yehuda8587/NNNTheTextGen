da8f440a753b1101761d5642a049256964c0eae5
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-08-12 23:21:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4405,6 +4405,7 @@ function updateForm(){
                                             upDatedSpaName = "Pura Vida Spa";
                                             upDatedSpaAddress = "Southcentre Mall, 100 Anderson Rd SE, Calgary, AB T2J 3V1" + "<br>" + "(Ground Floor, across from Sport Check)";
                                             upDatedSpaLINK = "https://forms.gle/w7fRjryo9sTSq5p27";
+                                            DatedSpaMapLINK = "https://maps.app.goo.gl/jBNQdzDcNS4N5GkY7";
                                             upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/pura-vida-skin-tightening-treatment-79-95";
                                             upDatedSpaPhone = "(587) 328-6478";
                                             upDatedDurationTotal = "45 Minutes";
