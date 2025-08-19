983c787f89c9e8514d669cddb46619c28eac00d2
Miriam-22him <miriamapv@noneedleneeded.com>
2025-08-19 15:55:59 -0600

    script.js

--- script.js
+++ script.js
@@ -5295,6 +5295,24 @@ function updateForm(){
                                     insteadOfSpa = "$377";
                                     break;
 
+
+                                     case "Triple N DTB Natural Facelift 79.95":
+                                    upDatedSpaName = "Triple N";
+                                    upDatedSpaAddress = "220 Technology Dr, Suite 110, Irvine, CA 92618,"+"<br>"+"(Corner office building on the first floor.)"+"<br>"+"Parking lot";
+                                    upDatedSpaLINK = "https://forms.gle/D2zbjrQS1F2Gy97c6";
+                                    upDatedSpaMapLINK = "https://maps.app.goo.gl/6XscTMpmrn379nJh9";
+                                    upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/triple-n-natural-facelift-79-95-dtb-2" ;
+                                    upDatedSpaPhone = "(714) 430-3899";
+                                    upDatedDurationTotal = "60 Minutes";
+                                    upDatedDurationOne = "- 15 minutes Cleansing";
+                                    upDatedDurationTwo = "- 30 minutes Natural Facelift and 15 minutes Consultation";
+                                    promoPrice = "79.95"
+                                    upDatedActualSpaPhone = "Can't find our spa? Give us a call at (714) 430-3899";
+                                    treatmentSelected = "Natural Facelift Treatment";
+                                    newSpaPhoneNumberSelected = "(714) 430-3899";
+                                    insteadOfSpa = "$377";
+                                    break;
+
                                     case "Triple N Neck and Jawline 59.95":
                                     upDatedSpaName = "Triple N";
                                     upDatedSpaAddress = "220 Technology Dr, Suite 110, Irvine, CA 92618,"+"<br>"+"(Corner office building on the first floor.)"+"<br>"+"Parking lot";
