35454f78cf7394fc135a9701c4ddffb79e061bd2
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-10-26 14:13:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16610,9 +16610,9 @@ case "Florentine Skincare and Cosmetics Natural Facelift $79.95":
                                 upDatedDurationOne = "- 15 minutes Cleansing and 35 minutes of Natural Facelift Treatment";
                                 upDatedDurationTwo = "- 15 minutes Consultation";
                                 promoPrice = "99.95";
-                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at ";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (585) 449-2100";
                                 treatmentSelected = "Natural Facelift Treatment";
-                                newSpaPhoneNumberSelected = "(438) 230-4017";
+                                newSpaPhoneNumberSelected = "(585) 449-2100";
                                 insteadOfSpa = "$377"
                                 break;
 
