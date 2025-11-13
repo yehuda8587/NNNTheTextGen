80aca3d0fd61aed822b057409e85d32c31ea2a76
Miriam-22him <miriamapv@noneedleneeded.com>
2025-11-13 13:18:08 -0600

    script.js

--- script.js
+++ script.js
@@ -12326,6 +12326,41 @@ function updateForm(){
                                         insteadOfSpa = "$377";
                                         break;
 
+
+                                    case "Skin Totale San Juan Natural Facelift $59.95":
+                                        upDatedSpaName = "Skin Totale Med Spa";
+                                        upDatedSpaAddress = "31878 Del Obispo Street, Suite 108, San Juan Capistrano, California 92675"+"<br>"+"(Inside of Plaza Del Obispo next to Thai Juan On restaurant)";
+                                        upDatedSpaLINK = "https://forms.gle/2hnUPuVpmGVKzPSEA";
+                                        upDatedSpaMapLINK = "https://maps.app.goo.gl/3Qs7nBrjtQ25gJwh7";
+                                        upDatedSpaWebLINK = "https://www.firstouchbeauty.com/pages/skin-totale-med-spa-san-juan-natural-facelift-59-95";
+                                        upDatedSpaPhone = "(949) 339-3541";
+                                        upDatedDurationTotal = "45 Minutes";
+                                        upDatedDurationOne = "- 30 minutes Skin Analysis, Deep Cleansing and Natural Facelift Treatment";
+                                        upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+                                        promoPrice = "59.95";
+                                        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (949) 514-6192";
+                                        treatmentSelected = "Natural Facelift Treatment";
+                                        newSpaPhoneNumberSelected = "(949) 514-6192";
+                                        insteadOfSpa = "$377";
+                                        break;
+
+                                        case "Skin Totale San Juan Neck and Jawline $59.95":
+                                        upDatedSpaName = "Skin Totale Med Spa";
+                                        upDatedSpaAddress = "31878 Del Obispo Street, Suite 108, San Juan Capistrano, California 92675"+"<br>"+"(Inside of Plaza Del Obispo next to Thai Juan On restaurant)";
+                                        upDatedSpaLINK = "https://forms.gle/2hnUPuVpmGVKzPSEA";
+                                        upDatedSpaMapLINK = "https://maps.app.goo.gl/3Qs7nBrjtQ25gJwh7";
+                                        upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/skin-totale-san-juan-neck-and-jawline-59-95";
+                                        upDatedSpaPhone = "(949) 339-3541";
+                                        upDatedDurationTotal = "45 Minutes";
+                                        upDatedDurationOne = "- 30 minutes Skin Analysis, Deep Cleansing and Neck and Jawline Treatment";
+                                        upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+                                        promoPrice = "59.95";
+                                        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (949) 514-6192";
+                                        treatmentSelected = "Neck and Jawline Treatment";
+                                        newSpaPhoneNumberSelected = "(949) 514-6192";
+                                        insteadOfSpa = "$377";
+                                        break;
+
                                         
 
                                         case "Skin Totale San Juan Men Skin Tightening $59.95":
