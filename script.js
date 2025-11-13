f0db4e0205da7905a2ffe3f0ab9088c074b3fcc6
Miriam-22him <miriamapv@noneedleneeded.com>
2025-11-13 10:33:03 -0600

    script.js

--- script.js
+++ script.js
@@ -16854,6 +16854,23 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
+                                case "Prime Clinic 3 in 1 Skin Tightening $59.95":
+                                upDatedSpaName = "Prime Clinic";
+                                upDatedSpaAddress = "Lakeland Square Mall, 3800 US Hwy 98 N, Lakeland, FL 33809"+"<br>"+"(It's just three stores down to the left of Bath & Body Works)";
+                                upDatedSpaLINK = "https://forms.gle/bTMnJHRcm4qTCNYUA";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/9fNmjJfbUTSd1zB9A";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/prime-clinic-3-in-1-skin-tightening-59-95";
+                                upDatedSpaPhone = "(863) 278-3069";
+                                upDatedDurationTotal = "60 Minutes";
+                                upDatedDurationOne = "- 45 minutes Cleansing and 3 in 1 Skin Tightening Treatment";
+                                upDatedDurationTwo = "- 15 minutes Consultation";
+                                promoPrice = "59.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (863) 777-0764";
+                                treatmentSelected = "3 in 1 Skin Tightening Treatment";
+                                newSpaPhoneNumberSelected = "(863) 777-0764";
+                                insteadOfSpa = "$377"
+                                break;
+
             
 
 
