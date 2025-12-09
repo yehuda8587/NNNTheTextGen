d57aab159921f001f0304e9c649033d078e0b96c
Miriam-22him <miriamapv@noneedleneeded.com>
2025-12-09 10:11:40 -0600

    script.js

--- script.js
+++ script.js
@@ -5640,6 +5640,24 @@ function updateForm(){
                                     insteadOfSpa = "$377";
                                     break;
 
+
+                                     case "Ben's Beauty Irvine Natural Collagen Restoration $79.95":
+                                    upDatedSpaName = "Ben's Beauty Irvine";
+                                    upDatedSpaAddress = "220 Technology Dr, Suite 110, Irvine, CA 92618,"+"<br>"+"(Corner office building on the first floor.)"+"<br>"+"Parking lot";
+                                    upDatedSpaLINK = "https://forms.gle/D2zbjrQS1F2Gy97c6";
+                                    upDatedSpaMapLINK = "https://maps.app.goo.gl/6XscTMpmrn379nJh9";
+                                    upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/bens-beauty-irvine-natural-collagen-restoration-79-95" ;
+                                    upDatedSpaPhone = "(714) 430-3899";
+                                    upDatedDurationTotal = "60 Minutes";
+                                    upDatedDurationOne = "- 15 minutes Cleansing";
+                                    upDatedDurationTwo = "- 30 minutes Natural Collagen Restoration and 15 minutes Consultation";
+                                    promoPrice = "79.95"
+                                    upDatedActualSpaPhone = "Can't find our spa? Give us a call at (949) 490-7726";
+                                    treatmentSelected = "Natural Collagen Restoration Treatment";
+                                    newSpaPhoneNumberSelected = "(949) 490-7726";
+                                    insteadOfSpa = "$377";
+                                    break;
+
                                     case "Ben's Beauty Irvine Natural Skin Tightening 79.95":
                                     upDatedSpaName = "Ben's Beauty Irvine";
                                     upDatedSpaAddress = "220 Technology Dr, Suite 110, Irvine, CA 92618,"+"<br>"+"(Corner office building on the first floor.)"+"<br>"+"Parking lot";
