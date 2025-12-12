e2609a38d13d10fba95fae147794c4b8a8fcea26
Miriam-22him <miriamapv@noneedleneeded.com>
2025-12-12 08:53:42 -0600

    script.js

--- script.js
+++ script.js
@@ -505,6 +505,24 @@ function updateForm(){
                     insteadOfSpa = "$377"
                     break;  
 
+
+                    case "Beauty Lounge Neck and Jawline 69.95":
+                    upDatedSpaName = "Beauty Lounge";
+                    upDatedSpaAddress = "3101 SW 34th Ave, #701, Ocala, FL 34474"+"<br>"+"(2 shops next to Ollie's Bargain Outlet,  Next to Christie Dental)";
+                    upDatedSpaLINK = "https://forms.gle/82TrFg5vLcFZQ4A37";
+                    upDatedSpaMapLINK = "https://maps.app.goo.gl/VsjjSdkkDXrGG9Mr5";
+                    upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/beauty-lounge-spa-neck-amp-amp-jawline-69-95";
+                    upDatedSpaPhone = "(352) 577-9216";
+                    upDatedDurationTotal = "60 Minutes";
+                    upDatedDurationOne = "- 15 Minutes Cleansing";
+                    upDatedDurationTwo = "- 45 Minutes Neck and Jawline Tightening Treatment & Consultation";
+                    promoPrice = "69.95"
+                    upDatedActualSpaPhone = "Can't find our spa? Give us a call at (352) 235-9336";
+                    treatmentSelected = "Neck and Jawline Tightening Treatment";
+                    newSpaPhoneNumberSelected = "(352) 235-9336";
+                    insteadOfSpa = "$377"
+                    break;  
+
                     case "Beauty Lounge Bye Bye Saggy Eyes 79.95":
                     upDatedSpaName = "Beauty Lounge";
                     upDatedSpaAddress = "3101 SW 34th Ave, #701, Ocala, FL 34474"+"<br>"+"(2 shops next to Ollie's Bargain Outlet,  Next to Christie Dental)";
