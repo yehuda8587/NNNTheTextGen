66b09dceb972fcc948d3a742e455a16cfe397c6c
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-12-15 17:31:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11799,6 +11799,23 @@ function updateForm(){
                                         insteadOfSpa = "$377";
                                         break;
 
+                                        case "Queen Bee Natural Facelift $79.95":
+                                        upDatedSpaName = "Queen Bee";
+                                        upDatedSpaAddress = "114 N Palm Canyon Dr. Palm Springs,  CA. 92262 United States"+"<br>"+"(Between Balboa Candy and Peepa's)"+"<br>"+"(In Front of Store)";
+                                        upDatedSpaLINK = "https://forms.gle/qvo1ebyuCpyd8xVcA";
+                                        upDatedSpaMapLINK = "https://maps.app.goo.gl/RFLZDKNDWpau1jAN9";
+                                        upDatedSpaWebLINK = "https://www.firstouchbeauty.com/pages/queen-bee-palm-springs-natural-facelift-79-95";
+                                        upDatedSpaPhone = "(760) 979-0516";
+                                        upDatedDurationTotal = "45 Minutes";
+                                        upDatedDurationOne = "- 10 minutes light cleansing";
+                                        upDatedDurationTwo = "- 35 minutes Natural Facelif Treatment and Consultation"
+                                        promoPrice = "79.95";
+                                        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (760) 459-3426";
+                                        treatmentSelected = "Natural Facelif Treatment";
+                                        newSpaPhoneNumberSelected = "(760) 459-3426";
+                                        insteadOfSpa = "$377";
+                                        break;
+
                                         case "La Reve LED 79.95":
                                         upDatedSpaName = "La Reve";
                                         upDatedSpaAddress = "Woodfield Mall,"+"<br>"+"5 Woodfield Mall Room F328 Schaumburg, IL 60173"+"<br>"+"(Second Floor, Near Sephora)"+"<br>"+"(Park near main entrance)";
