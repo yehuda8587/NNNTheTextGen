0b101c02b26fd9b6db6b5b01919594c1931df659
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2025-12-18 12:24:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11010,6 +11010,23 @@ function updateForm(){
                                     insteadOfSpa = "$377";
                                     break;
 
+                                    case "Oasis Aesthetics Henderson Hydraglow 59.95":
+                                    upDatedSpaName = "Oasis Aesthetics";
+                                    upDatedSpaAddress = "1919 Henderson Hwy, Winnipeg MB, R2G1P4"+"<br>"+"(River East)"+"<br>"+"(Parking is available just in front of the spa)";
+                                    upDatedSpaLINK = "https://forms.gle/X8CkCCRLLaFTSzqZ9";
+                                    upDatedSpaMapLINK = "https://maps.app.goo.gl/yAq2WXa65WyrFnnG6";
+                                    upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/oasis-henderson-hydraglow-59-95";
+                                    upDatedSpaPhone = "(204) 800-4409";
+                                    upDatedDurationTotal = "75 Minutes";
+                                    upDatedDurationOne = "- 60 minutes cleansing and Hydraglow Treatment";
+                                    upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+                                    promoPrice = "59.95";
+                                    upDatedActualSpaPhone = "Can't find our spa? Give us a call at (204) 691-1922";
+                                    treatmentSelected = " Hydraglow Treatment";
+                                    newSpaPhoneNumberSelected = "(204) 691-1922";
+                                    insteadOfSpa = "$377";
+                                    break;
+
                                     case "Oasis Aesthetics Henderson Bye Bye Saggy Eyes 49.95":
                                     upDatedSpaName = "Oasis Aesthetics";
                                     upDatedSpaAddress = "1919 Henderson Hwy, Winnipeg MB, R2G1P4"+"<br>"+"(River East)"+"<br>"+"(Parking is available just in front of the spa)";
