7141f120c29daed1365eb2b47967f372632d36c1
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2026-01-05 15:26:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16504,6 +16504,23 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
+                                case "Velora Med Spa Men's Skin Tightening $99.95":
+                                upDatedSpaName = "Velora Med Spa";
+                                upDatedSpaAddress = "108 N State St, Chicago, IL 60602, United States"+"<br>"+"(Block 37, First level)";
+                                upDatedSpaLINK = "https://forms.gle/m4QFVnRBasHH7UqS9";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/dV13uj69NZsR9zXZ9";
+                                upDatedSpaWebLINK = "https://www.advancedbeautytreatments.com/pages/velora-med-spa-mens-skin-tightening-99-95";
+                                upDatedSpaPhone = "(312) 625-1275";
+                                upDatedDurationTotal = "45 Minutes";
+                                upDatedDurationOne = " - 15 minutes Cleansing";
+                                upDatedDurationTwo = " - 30 minutes Skin Tightening & Consultation";
+                                promoPrice = "99.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (331) 600-2304";
+                                treatmentSelected = "Natural Facelift";
+                                newSpaPhoneNumberSelected = "(331) 600-2304";
+                                insteadOfSpa = "$377"
+                                break;
+
                                 case "Velora Med Spa Natural Facelift $79.95":
                                 upDatedSpaName = "Velora Med Spa";
                                 upDatedSpaAddress = "108 N State St, Chicago, IL 60602, United States"+"<br>"+"(Block 37, First level)";
