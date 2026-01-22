2d9dc1f2814b0ee8b2c91323fe42508200595fc2
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2026-01-22 09:11:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14066,6 +14066,23 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
+                                case "Moana Crystals Natural Skin Tightening $59.95":
+                                upDatedSpaName = "Moana Crystals";
+                                upDatedSpaAddress = "2250 Kalakaua Ave. Honolulu, HI 96815"+"<br>"+"(Waikiki shopping plaza, 2nd floor next to the elevator, Room 217)";
+                                upDatedSpaLINK = "https://forms.gle/17kGFgn6KXJcs3xY7";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/fFYEqgHUbmanrLiR6";
+                                upDatedSpaWebLINK = "https://moana.advancedbeautytreatments.com/natural-skin-tightening-59-95-form";
+                                upDatedSpaPhone = "(808) 764-3367";
+                                upDatedDurationTotal = "60 Minutes";
+                                upDatedDurationOne = "- 10 minutes Cleansing";
+                                upDatedDurationTwo = "- 40 minutes Natural Skin Tightening Treatment and 10 minutes post-care and consultation";
+                                promoPrice = "59.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (808) 779-8279";
+                                treatmentSelected = "Natural Skin Tightening Treatment";
+                                newSpaPhoneNumberSelected = "(808) 779-8279";
+                                insteadOfSpa = "$377"
+                                break;
+
 
                                 case "Moana Crystals Neck and Jawline $59.95":
                                 upDatedSpaName = "Moana Crystals";
@@ -14083,9 +14100,6 @@ function updateForm(){
                                 newSpaPhoneNumberSelected = "(808) 779-8279";
                                 insteadOfSpa = "$377"
                                 break;
-
-
-            
 
 
                                 case "Miliv Med Spa Natural Facelift $79.95":
