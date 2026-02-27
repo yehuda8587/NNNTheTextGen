5da266b5f832702408e8711cdb9f8974d580c6a4
Miriam-22him <miriamapv@noneedleneeded.com>
2026-02-26 22:16:42 -0600

    script.js

--- script.js
+++ script.js
@@ -15814,6 +15814,23 @@ function updateForm(){
                                 insteadOfSpa = "$377"
                                 break;
 
+                                case "Velora Med Spa Triple Lift $99.95":
+                                upDatedSpaName = "Velora Med Spa";
+                                upDatedSpaAddress = "108 N State St, Chicago, IL 60602, United States"+"<br>"+"(Block 37, First level)";
+                                upDatedSpaLINK = "https://forms.gle/m4QFVnRBasHH7UqS9";
+                                upDatedSpaMapLINK = "https://maps.app.goo.gl/dV13uj69NZsR9zXZ9";
+                                upDatedSpaWebLINK = "https://velora.advancedbeautytreatments.com/triple-lift-booking";
+                                upDatedSpaPhone = "(312) 625-1275";
+                                upDatedDurationTotal = "45 Minutes";
+                                upDatedDurationOne = " - 15 minutes Cleansing";
+                                upDatedDurationTwo = " - 30 minutes Triple Lift & Consultation";
+                                promoPrice = "99.95";
+                                upDatedActualSpaPhone = "Can't find our spa? Give us a call at (331) 600-2304";
+                                treatmentSelected = "Triple Lift";
+                                newSpaPhoneNumberSelected = "(331) 600-2304";
+                                insteadOfSpa = "$377"
+                                break;
+
                                 case "Velora Med Spa Natural Facelift $79.95":
                                 upDatedSpaName = "Velora Med Spa";
                                 upDatedSpaAddress = "108 N State St, Chicago, IL 60602, United States"+"<br>"+"(Block 37, First level)";
