940a7cb9a7f2e9d185146a8eed2ff48c2bdcc74d
Miriam-22him <miriamapv@noneedleneeded.com>
2026-03-07 12:48:31 -0600

    script.js

--- script.js
+++ script.js
@@ -17102,7 +17102,7 @@ case "Florentine Skincare and Cosmetics Natural Facelift $79.95":
                                 insteadOfSpa = "$377"
                                 break;
 
-                                ase "Lumera Skin Clinics Hair Removal $99.95":
+                                case "Lumera Skin Clinics Hair Removal $99.95":
                                 upDatedSpaName = "Lumera Skin Clinics";
                                 upDatedSpaAddress = "1216 Rue Stanley, Montréal, QC H3B 2S7"+"<br>"+"(Inside the building on the second floor)";
                                 upDatedSpaLINK = "https://forms.gle/u9QW8E8ExtwsFChG7";
