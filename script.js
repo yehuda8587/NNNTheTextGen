43d5bc16a4f9ad875acef29c566bbaa4df582ac3
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2026-04-29 16:05:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ function updateForm(){
         upDatedSpaAddress = "5111 N Scottsdale Rd Suite 208, Scottsdale, AZ 85250"+"<br>"+"(Inside a medical building)";
         upDatedSpaLINK = "https://forms.gle/EYztXbdBab5JqdBt8";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/eeRnVJMPibJpQhjW7";
-        upDatedSpaWebLINK = "https://medlaser-scot.firstouchbeauty.com/lift-and-sculpt-89-95-book-page";
+        upDatedSpaWebLINK = "https://medlaser-scot.firstouchbeauty.com/natural-facelift-69-95-book-page";
         upDatedSpaPhone = "(602) 830-1559";
         upDatedDurationTotal = "60 Minutes";
         upDatedDurationOne = " - ";
@@ -299,12 +299,12 @@ function updateForm(){
         insteadOfSpa = "$377"
         break;
 
-        case "Arizona MedLaser Glendale Natural Facelift $89.95":
+        case "Arizona MedLaser Glendale Natural Facelift $69.95":
         upDatedSpaName = "Arizona MedLaser";
         upDatedSpaAddress = "5701 W Talavi Blvd Suite 101, Glendale, AZ 85306"+"<br>"+"(Inside a medical building)";
         upDatedSpaLINK = "https://forms.gle/cGjvr4ahGDZ8Zi536";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/6BpdFWQDrSiVv8c39";
-        upDatedSpaWebLINK = "https://medlaser-glen.firstouchbeauty.com/natural-facelift-89-95-book-page";
+        upDatedSpaWebLINK = "https://medlaser-glen.firstouchbeauty.com/natural-facelift-69-95-book-page";
         upDatedSpaPhone = "(602) 812-2910";
         upDatedDurationTotal = "60 Minutes";
         upDatedDurationOne = " - 15 minutes Cleansing";
