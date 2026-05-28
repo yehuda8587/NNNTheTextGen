0050e371958956f0d0e0145ffe270be0a02745bb
Miriam-22him <miriamapv@noneedleneeded.com>
2026-05-28 12:05:50 -0600

    script.js

--- script.js
+++ script.js
@@ -5582,6 +5582,23 @@ function updateForm(){
         insteadOfSpa = "$377";
         break;
 
+        case "Skin Totale 3D Facelift $59.95":
+        upDatedSpaName = "Skin Totale";
+        upDatedSpaAddress = "309 E Katella Avenue, Orange, CA 92867"+"<br>"+"(Inside Katella Shopping Center, behind Marie Callender's Restaurant & Bakery, near 'Train with Dave')";
+        upDatedSpaLINK = " ";
+        upDatedSpaMapLINK = "https://maps.app.goo.gl/f11qLG86Ze123w1aA";
+        upDatedSpaWebLINK = "https://skintotale.firstouchbeauty.com/3d-facelift-booking";
+        upDatedSpaPhone = "(714) 450-6547";
+        upDatedDurationTotal = "60 Minutes";
+        upDatedDurationOne = "- 45 minutes Skin Analysis and 3D Facelift Treatment";
+        upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+        promoPrice = "59.95";
+        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (714) 852-5300";
+        treatmentSelected = "3D Facelift Treatment";
+        newSpaPhoneNumberSelected = "(714) 852-5300";
+        insteadOfSpa = "$377";
+        break;
+
         case "Skin Totale Lift and Hydrate $59.95":
         upDatedSpaName = "Skin Totale";
         upDatedSpaAddress = "309 E Katella Avenue, Orange, CA 92867"+"<br>"+"(Inside Katella Shopping Center, behind Marie Callender's Restaurant & Bakery, near 'Train with Dave')";
