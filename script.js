223db3cfd4a37aba98f343c704fe3e36e0b76bdd
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2026-06-04 09:54:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5719,6 +5719,23 @@ function updateForm(){
         insteadOfSpa = "$377";
         break;
 
+        case "Skin Totale San Juan Men's Facial $59.95":
+        upDatedSpaName = "Skin Totale Med Spa";
+        upDatedSpaAddress = "31878 Del Obispo Street, Suite 108, San Juan Capistrano, California 92675"+"<br>"+"(Inside of Plaza Del Obispo next to Thai Juan On restaurant)";
+        upDatedSpaLINK = "https://forms.gle/2hnUPuVpmGVKzPSEA";
+        upDatedSpaMapLINK = "https://maps.app.goo.gl/3Qs7nBrjtQ25gJwh7";
+        upDatedSpaWebLINK = "https://skintotalesj.noneedleneeded.com/men-facial-booking-59-95";
+        upDatedSpaPhone = "(949) 339-3541";
+        upDatedDurationTotal = "60 Minutes";
+        upDatedDurationOne = "- 45 minutes Skin Analysis, Cleansing and Men's Facial Treatment";
+        upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+        promoPrice = "59.95";
+        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (949) 514-6192";
+        treatmentSelected = "Men's Facial Treatment";
+        newSpaPhoneNumberSelected = "(949) 514-6192";
+        insteadOfSpa = "$377";
+        break;
+
         case "Skin Totale San Juan Anti-Aging Facial $59.95":
         upDatedSpaName = "Skin Totale Med Spa";
         upDatedSpaAddress = "31878 Del Obispo Street, Suite 108, San Juan Capistrano, California 92675"+"<br>"+"(Inside of Plaza Del Obispo next to Thai Juan On restaurant)";
@@ -6672,7 +6689,7 @@ function updateForm(){
         upDatedSpaAddress = "Governor Square Mall"+"<br>"+"1500 Apalachee Pkwy, Unit 2350, Tallahassee, FL 32301"+"<br>"+"(Second floor next to Victoria's Secret)";
         upDatedSpaLINK = "https://forms.gle/d9bVoE3hSMELQ7a47";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/dHywqo21iYKv9GV16";
-        upDatedSpaWebLINK = "https://tresorare.firstouchbeauty.com/natural-facelift-79-95-book-page";
+        upDatedSpaWebLINK = "https://tresorare.firstouchbeauty.com/natural-facelift-79-95-booking";
         upDatedSpaPhone = "(754) 258-3309";
         upDatedDurationTotal = "45 Minutes";
         upDatedDurationOne = " - 10 minutes Cleansing";
