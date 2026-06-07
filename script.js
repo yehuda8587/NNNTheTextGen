04ec23f6b46e98cfb9d8359e6fb610421203441d
mongarcia06 <138028001+mongarcia06@users.noreply.github.com>
2026-06-07 14:00:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5702,6 +5702,23 @@ function updateForm(){
         insteadOfSpa = "$377";
         break;
 
+        case "Skin Totale San Juan 3D Facelift $59.95":
+        upDatedSpaName = "Skin Totale Med Spa";
+        upDatedSpaAddress = "31878 Del Obispo Street, Suite 108, San Juan Capistrano, California 92675"+"<br>"+"(Inside of Plaza Del Obispo next to Thai Juan On restaurant)";
+        upDatedSpaLINK = "https://forms.gle/2hnUPuVpmGVKzPSEA";
+        upDatedSpaMapLINK = "https://maps.app.goo.gl/3Qs7nBrjtQ25gJwh7";
+        upDatedSpaWebLINK = "https://skintotalesj.noneedleneeded.com/3d-facelift-booking-page";
+        upDatedSpaPhone = "(949) 339-3541";
+        upDatedDurationTotal = "60 Minutes";
+        upDatedDurationOne = "- 45 minutes Skin Analysis, Deep Cleansing and 3D Facelift Treatment";
+        upDatedDurationTwo = "- 15 minutes Post Care and Consultation"
+        promoPrice = "59.95";
+        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (949) 514-6192";
+        treatmentSelected = "3D Facelift Treatment";
+        newSpaPhoneNumberSelected = "(949) 514-6192";
+        insteadOfSpa = "$377";
+        break;
+
         case "Skin Totale Natural Facelift $59.95":
         upDatedSpaName = "Skin Totale";
         upDatedSpaAddress = "309 E Katella Avenue, Orange, CA 92867"+"<br>"+"(Inside Katella Shopping Center, behind Marie Callender's Restaurant & Bakery, near 'Train with Dave')";
