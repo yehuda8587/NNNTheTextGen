955340bb237aa2fe78cce63e4e83efc3e4fa9f8c
Miriam-22him <miriamapv@noneedleneeded.com>
2026-07-24 09:40:11 -0600

    Add details for Oasis Aesthetics Body Contouring

--- script.js
+++ script.js
@@ -4673,6 +4673,24 @@ function updateForm(){
         insteadOfSpa = "$599";
         break;
 
+        case "Oasis Aesthetics St. Mary's Body Contouring 59.95":
+        upDatedSpaName = "Oasis Aesthetics";
+        upDatedSpaAddress = "1026 St Mary's Rd Unit A Winnipeg, MB R2M 3S6"+"<br>"+"(Unit A)";
+        upDatedSpaLINK = "https://forms.gle/sSQUXR3kFgoN5vQc6";
+        upDatedSpaMapLINK = "https://maps.app.goo.gl/yJomamRPC2hTi7Tg8";
+        upDatedSpaWebLINK = "https://oasis-stmary.noneedleneeded.com/body-contouring-59-95-booking";
+        upDatedSpaPhone = "(204) 819-5651";
+        upDatedDurationTotal = "60 Minutes";
+        upDatedDurationOne = "- 45 minutes of Body Contouring Treatment";
+        upDatedDurationTwo = "- 15 minutes Consultation"
+        promoPrice = "59.95";
+        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (431) 570-2004";
+        treatmentSelected = "Body Contouring Treatment";
+        newSpaPhoneNumberSelected = "(431) 570-2004";
+        insteadOfSpa = "$377";
+        break;
+
+
         case "Oasis Aesthetics St. Mary's Mommy Tummy Rescue 69.95":
         upDatedSpaName = "Oasis Aesthetics";
         upDatedSpaAddress = "1026 St Mary's Rd Unit A Winnipeg, MB R2M 3S6"+"<br>"+"(Unit A)";
