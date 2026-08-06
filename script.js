20310d38deb39848faf145e315197b1fb450bf69
Miriam-22him <miriamapv@noneedleneeded.com>
2026-08-06 12:17:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4874,7 +4874,24 @@ function updateForm(){
         upDatedDurationTwo = "- 15 minutes Consultation"
         promoPrice = "49.95";
         upDatedActualSpaPhone = "Can't find our spa? Give us a call at (431) 570-2004";
-        treatmentSelected = "Hiperpigmentation Treatment Treatment";
+        treatmentSelected = "Hiperpigmentation Treatment";
+        newSpaPhoneNumberSelected = "(431) 570-2004";
+        insteadOfSpa = "$377";
+        break;
+
+        case "Oasis Aesthetics St. Mary's 3-in-1 Tightening & Brightening 49.95":
+        upDatedSpaName = "Oasis Aesthetics";
+        upDatedSpaAddress = "1026 St Mary's Rd Unit A Winnipeg, MB R2M 3S6"+"<br>"+"(Unit A)";
+        upDatedSpaLINK = "https://forms.gle/sSQUXR3kFgoN5vQc6";
+        upDatedSpaMapLINK = "https://maps.app.goo.gl/yJomamRPC2hTi7Tg8";
+        upDatedSpaWebLINK = "https://oasis-stmary.noneedleneeded.com/3-in-1-tightening-brightening-49-95-booking";
+        upDatedSpaPhone = "(204) 819-5651";
+        upDatedDurationTotal = "60 Minutes";
+        upDatedDurationOne = "- 45 minutes cleansing and 3-in-1 Tightening & Brightening Treatment";
+        upDatedDurationTwo = "- 15 minutes Consultation"
+        promoPrice = "49.95";
+        upDatedActualSpaPhone = "Can't find our spa? Give us a call at (431) 570-2004";
+        treatmentSelected = "3-in-1 Tightening & Brightening Treatment";
         newSpaPhoneNumberSelected = "(431) 570-2004";
         insteadOfSpa = "$377";
         break;
