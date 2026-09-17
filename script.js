e03dbc6573bfdbfbcdbffbe279149fb9d408fd26
Miriam-22him <miriamapv@noneedleneeded.com>
2026-09-17 13:06:07 -0600

    Refine spa address for multiple services
    Updated the spa address details for various services to provide clearer directions and information.

--- script.js
+++ script.js
@@ -1756,7 +1756,7 @@ function updateForm(){
 
         case "Florentine Skincare and Cosmetics Natural Facelift $79.95":
         upDatedSpaName = "Florentine Skincare and Cosmetics";
-        upDatedSpaAddress = "Florentine Skincare and Cosmetics"+"<br>"+"Inside Sherway Gardens"+"<br>"+"25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(We are located Inside the mall. When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and  Fox Clothing.)";
+        upDatedSpaAddress = "We are located inside Sherway Gardens Mall at street 25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and Fox Clothing.)";
         upDatedSpaLINK = "https://forms.gle/E8975rmoLv4NpuAy8";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/9VTTUe4Hi8nNWocv8";
         upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/florentine-skincare-amp-cosmetics-natural-facelift-79-95";
@@ -1773,7 +1773,7 @@ function updateForm(){
 
         case "Florentine Skincare and Cosmetics Triple Lift $69.00":
         upDatedSpaName = "Florentine Skincare and Cosmetics";
-        upDatedSpaAddress = "Florentine Skincare and Cosmetics"+"<br>"+"Inside Sherway Gardens"+"<br>"+"25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(We are located Inside the mall. When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and  Fox Clothing.)";
+        upDatedSpaAddress = "We are located inside Sherway Gardens Mall at street 25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and Fox Clothing.)";
         upDatedSpaLINK = "https://forms.gle/E8975rmoLv4NpuAy8";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/9VTTUe4Hi8nNWocv8";
         upDatedSpaWebLINK = "https://florentine.noneedleneeded.com/triple-lift-69-booking";
@@ -1790,7 +1790,7 @@ function updateForm(){
 
         case "Florentine Skincare and Cosmetics Natural Facelift $99.95":
         upDatedSpaName = "Florentine Skincare and Cosmetics";
-        upDatedSpaAddress = "Florentine Skincare and Cosmetics"+"<br>"+"Inside Sherway Gardens"+"<br>"+"25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(We are located Inside the mall. When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and  Fox Clothing.)";
+        upDatedSpaAddress = "We are located inside Sherway Gardens Mall at street 25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and Fox Clothing.)";
         upDatedSpaLINK = "https://forms.gle/E8975rmoLv4NpuAy8";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/9VTTUe4Hi8nNWocv8";
         upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/florentine-skincare-and-cosmetics-natural-facelift-99-95";
@@ -1807,7 +1807,7 @@ function updateForm(){
 
         case "Florentine Skincare and Cosmetics Neck and Jawline $79.95":
         upDatedSpaName = "Florentine Skincare and Cosmetics";
-        upDatedSpaAddress = "Florentine Skincare and Cosmetics"+"<br>"+"Inside Sherway Gardens"+"<br>"+"25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(We are located Inside the mall. When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and  Fox Clothing.)";
+        upDatedSpaAddress = "We are located inside Sherway Gardens Mall at street 25 The West Mall, Etobicoke, ON M9C 1B8, Canadá"+"<br>"+"(When you arrive at the mall, please enter through Entrance 4. Once you enter, you will see Scotiabank. Turn left, and you’ll find the store on your right hand side between Yogen Fruz and Fox Clothing.)";
         upDatedSpaLINK = "https://forms.gle/E8975rmoLv4NpuAy8";
         upDatedSpaMapLINK = "https://maps.app.goo.gl/9VTTUe4Hi8nNWocv8";
         upDatedSpaWebLINK = "https://www.noneedleneeded.com/pages/florentine-skincare-and-cosmetics-neck-amp-jawline-79-95";
